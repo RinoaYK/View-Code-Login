1d130fc15bf224979e73859ff9b2bc45dff29e0a
Lidia <rinoayk@gmail.com>
2025-10-16 12:07:19 -0300

    Initial Commit

---
+++ LoginViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginViewCode
+//
+//  Created by Lidia on 16/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LoginViewCodeTests/LoginViewCodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewCodeTests.swift
+//  LoginViewCodeTests
+//
+//  Created by Lidia on 16/10/25.
+//
+
+import Testing
+@testable import LoginViewCode
+
+struct LoginViewCodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
